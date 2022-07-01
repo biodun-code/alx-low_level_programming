2582f02979fdcb8fd173c693f826e5541c7d521e
majorshuga <biodunbio14@gmail.com>
2022-07-01 13:35:26 -0700

    l

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 int main(void)
 {
 	char c = 'a';
-while (c <= 'z')
+	while (c <= 'z')
 {
-	putchar(c);
-	c++;									}
+putchar(c);									c++;										}
 c = 'A';
 while (c <= 'Z')
-{										putchar(c);
+putchar(c);
 c++;
+}
 putchar('\n');
 return (0);
 }
