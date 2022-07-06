35a7cf90e997e1462a74ee2ef2ae014cba817ba3
majorshuga <biodunbio14@gmail.com>
2022-07-06 14:12:55 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,13 @@
 /**
 *print_alphabet -> prints the lowercase alphabets
 */
-
-
 void print_alphabet(void);
 {
 int j;
 
 for (j = 'a'; j <= 'z'; j++)
 {
-_putchar(j)
+ _putchar(j)
 }
-_putchar('\n);
+ _putchar('\n);
 }
