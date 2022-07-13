84d3685dfc8204df474fed0f4ace81277a7169a4
majorshuga <biodunbio14@gmail.com>
2022-07-13 13:28:54 +0100

    l

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,14 @@
 
 void puts_half(char *str)
 {
-	int i;
+int i;
+for (i = 0; str[i] != '\0'; i++)
+;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	i++;
-	for (i /= 2; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+i++;
+for (i /= 2; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
