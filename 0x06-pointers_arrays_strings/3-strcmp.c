9f976a0f2fe6015318b32332596fbac45e6bf1ae
majorshuga <biodunbio14@gmail.com>
2022-07-14 17:05:20 +0100

    k

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,13 @@ int i;
 
 /**
  * We have reached the end of S1 OR we have reached the end of S2
- * 
 */
 
-for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++);
+for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 {
-	if (s1[i] != s2[i])
+if (s1[i] != s2[i])
 {
-		return (s1[i] - s2[i]);
+return (s1[i] - s2[i]);
 }
 }
 return (0);
