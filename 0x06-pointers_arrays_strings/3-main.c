98037e8b78f0e3b3311e2d439338ebeac6c321ca
majorshuga <biodunbio14@gmail.com>
2022-07-14 16:37:09 +0100

    l

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -10,6 +10,7 @@ int main(void)
 {
 char s1[] = "Hello";
 char s2[] = "World!";
+
 printf("%d\n", _strcmp(s1, s2));
 printf("%d\n", _strcmp(s2, s1));
 printf("%d\n", _strcmp(s1, s1));
