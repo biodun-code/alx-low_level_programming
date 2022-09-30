61f0e14d018df79e0c564414531018aad4e56eb3
majorshuga <biodunbiodun0000@gmail.com>
2022-09-30 11:35:44 +0100

    l

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,15 +10,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nbr_nodes = 0;
-	const dlistint_t *current = h;
-
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nbr_nodes++;
-	}
-
-	return (nbr_nodes);
+size_t nbr_nodes = 0;
+const dlistint_t *current = h;
+while (current != NULL)
+{
+printf("%i\n", current->n);
+current = current->next;
+nbr_nodes++;
+}
+return (nbr_nodes);
 }
